949887a2b7aae809991639f898d4096a21d5241b
yassinehamza1996 <yassine.hamza@etudiant-fst.utm.tn>
2020-12-23 21:09:50 +0100

    initialized

---
+++ src/test/java/com/vermeg/ams/HellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vermeg.ams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
